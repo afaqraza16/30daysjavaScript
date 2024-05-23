40bc8f2c3f746cfaedcfe07f84e70c105a854422
afaqraza16 <afaqraza0220@gmail.com>
2024-05-24 02:44:58 +0500

    This is Day of JS

---
+++ script.js
@@ -0,0 +1,122 @@
+// // This is the Day 1 class
+
+// //  it runs directly in the Browsers
+// console.log('afaq raza');
+
+// document.write("This is Day 1 Javascript class");
+// // Variables
+// // it can  change with respect to time 
+// // Number Values
+// age = 23;
+// console.log(age)
+// // after one year
+// age = age + 1;
+// console.log(age)
+// // String values
+// name = 'afaq';
+// console.log(name)
+// name = 'afaq raza';
+// console.log(name)
+// Null 
+// nullValue= null;
+// console.log(nullValue)
+// // undefined values
+// undefinedValue = undefined
+// console.log(undefinedValue)
+// // Boolean Values
+// isFollow = true;
+// console.log(isFollow)
+
+// // Roles For Variables
+// // Case Sensitive fullName
+// // camelCase
+// // Reserved words can not be used as a variables
+// // underscore dollar sign ($) is allowed with out a space 
+
+// // some cases
+// // full-name // kabab case
+// // full_name //snake case
+// // fullName //camelCase
+// // FullName  //pascal Case
+
+// // Variables let const& var
+
+// let FullName = 'Azmat Sir';
+// let age = 40;
+// // let isFollow = true;
+// console.log(FullName)
+// console.log(age);
+// console.log(isFollow)
+
+// // var we can redeclare
+// var name = 'Azmat Sir';
+
+// var name = 'Syed Azmat Shah';
+// console.log( name  );
+
+
+// // var kayword can be redaclre with var keyword and also it is a global scope
+
+// var a = 12;
+// var a = 20;
+// console.log(a)
+// {
+//     var a = 30;
+//     console.log(a)
+// }
+// let radius = 3.14;
+// // we can not redaclare the let values with let keyword
+// // let radius = 3.13;
+// console.log(radius)
+// // let is a Block scope
+// {
+//    let  b= 100;
+//    console.log(b); 
+// }
+// {
+//  let country = "pakistan";
+//  console.log(country)
+// }
+// // const can't be redeclare and can not be reassign 
+// const pi = 3.14;
+// console.log(pi)
+// // const is a block scope
+// {
+//     const country = "pakistan";
+//     console.log(country)
+//     const radius = 14;
+//     console.log(radius)
+// }
+// const isFollow =  true;
+// console.log(true);
+ 
+// let obj = {
+//     fullName : "Waseem",
+//     age : 20,
+//     isFollow : true,
+//     address : "Peshawar"
+// }
+// console.log(obj)
+// let data =  100;
+// console.log(data.toString())
+// console.log((100).toString())
+// let person = {
+//     name :"waseem"
+// }
+// const member = person;
+// person = null;
+// console.log(member);
+
+// let num = 20;
+// let b = num+"20";
+// console.log(b++);
+
+// console.log(b)
+
+
+// let arr1 = [1,2,3,4,5,6,77];
+// // console.log(arr1)
+// let arr2= {
+//     name : "waseem",
+//     age : 20
+// }
